2c87af2a8fc3d29e6ebf762fde3b17890ffcd8a5
rockdonald2 <rockdonald2@gmail.com>
2020-08-05 22:52:05 +0300

    2020.08.05

--- static/js/viz_main.js
+++ static/js/viz_main.js
@@ -5,9 +5,10 @@
         .defer(d3.json, 'static/data/combined.json')
         .defer(d3.json, 'static/data/codes.json')
         .defer(d3.json, 'static/data/regions.json')
+        .defer(d3.json, 'static/data/map.json')
         .await(ready);
 
-    function ready (error, combinedData, codesData, regionsData) {
+    function ready (error, combinedData, codesData, regionsData, mapData) {
         /* ha valamilyen hiba történt álljon le */
         if (error) {
             return console.warn(error);
@@ -16,6 +17,7 @@
         viz.data.scores = combinedData;
         viz.data.codes = codesData;
         viz.data.regions = regionsData;
+        viz.data.map = mapData;
         viz.makeFilterAndDimension(viz.makeDataByCountry());
 
         viz.init();
